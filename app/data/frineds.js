6ebe871cac2e13c8eeba02e609b704a1b3b8693d
Androw <androw@MacBook-Pro-2.attlocal.net>
2018-10-18 16:21:08 -0400

    first-init

---
+++ app/data/frineds.js
@@ -0,0 +1,101 @@
+var friends = [{
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Phoebe Buffay",
+        photo: "http://25.media.tumblr.com/23e479224f5179ac42665e7a648cf309/tumblr_mndv0cYih81r9zaago1_500.gif",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Monica Geller",
+        photo: "https://media.giphy.com/media/beYVvBUHy1Gla/giphy.gif",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Chandler Bing",
+        photo: "http://33.media.tumblr.com/cf9686363fde6ca698a9b5b603eef01a/tumblr_ms0bxt7ThZ1r9zaago1_500.gif",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+
+];
+
+
+module.exports = friends;
